16c5b492f78cfa1cacb878288881f5885173eaf0
GeneLil <evgeniy.lilik@gmail.com>
2019-08-20 23:53:57 +0300

    Modified makefile, package.json, gitignore

--- Makefile
+++ Makefile
@@ -1,4 +1,6 @@
 install:
-	yarn install
+	npm install
 start:
 	npx babel-node src/bin/brain-games.js
+publish:
+	npm publish --dry-run
--- babel.config.js
+++ babel.config.js
@@ -1,10 +1,11 @@
-{
-	"presets": [
-		["@babel/preset-env",
-			{
-				"targets": {
-					"node": "current"
-				}
-			}
-	]
+module.exports = {
+  "presets": [
+    ["@babel/preset-env",
+      {
+        "targets": {
+          "node": "current"
+        },
+      },
+    ],
+  ],
 }
